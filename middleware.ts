e751d8c92c21ca2f6815ee2753f26165101999b8
Eliab Silveira <eliaberfsilveira@gmail.com>
2026-05-27 21:08:42 -0300

    fix: remove redirect loop no middleware

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,10 @@
-import { NextResponse, type NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: [],
+  matcher: '/admin/:path*',
 }
